4a55371511397134c5186e0cb75f048a9332038c
Mercy ADEMIJU <mercyademiju24@gmail.com>
2022-04-29 12:21:20 +0100

    feat: Added logic to make submit button clear colored tiles

--- designs.js
+++ designs.js
@@ -1,16 +1,37 @@
 // Select color input
 const colorInput = document.getElementById("colorPicker");
-// Select size input
+// Select size width input === <td></td>
 const widthInput = document.getElementById("inputWidth"); //td
+// Select size height input === <tr></tr>
 const heightInput = document.getElementById("inputHeight"); //tr
+// Select table
 const pixelCanvas = document.getElementById("pixelCanvas");
-
+// Select the form
 const formSubmit = document.getElementById("sizePicker");
 
-function makeGrid() {
-    for (let row = 0; row < heightInput.value; row++) {
+formSubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const widthInput = document.getElementById("inputWidth").value;
+    const heightInput = document.getElementById("inputHeight").value;
+    makeGrid(heightInput, widthInput);
+
+    //to  access the child node of <table></table> which returns  HTMLCollection
+    for (let i = 0; i < pixelCanvas.lastElementChild.children.length; i++) {
+        // to access the children of <tr></tr> which returns and HTMLCollection
+        for (
+            let j = 0; j < pixelCanvas.lastElementChild.children[i].children.length; j++
+        ) {
+            const add = pixelCanvas.lastElementChild.children[i].children[j];
+            add.style.removeProperty("background-color");
+        }
+    }
+});
+
+function makeGrid(heightInput, widthInput) {
+    pixelCanvas.innerHTML = "";
+    for (let row = 0; row < heightInput; row++) {
         const createRow = pixelCanvas.insertRow(-1);
-        for (let column = 0; column < widthInput.value; column++) {
+        for (let column = 0; column < widthInput; column++) {
             const createColumn = createRow.insertCell(-1);
             createColumn.addEventListener("mousedown", () => {
                 createColumn.style.backgroundColor = colorInput.value;
@@ -18,8 +39,3 @@ function makeGrid() {
         }
     }
 }
-
-formSubmit.addEventListener("submit", (e) => {
-    e.preventDefault();
-    makeGrid();
-});
